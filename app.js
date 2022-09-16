d93e103c30121bc3c7f2f964e8b66e04863f841f
Luis Ascencio <luiscarlosasqu@ufps.edu.co>
2022-09-15 19:35:12 -0500

    Solucionando problemas <3

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //Barra de Navegación
 var nav = document.querySelector('.nav_cont'); 
 var sec = document.querySelector('#seccion_a'); 
-var sec2 = document.querySelector('#seccion_b'); 
 var sec3 = document.querySelector('#seccion_c'); 
 
 
@@ -10,7 +9,7 @@ window.addEventListener('scroll', function(event) {
         nav.style.backgroundColor = 'transparent'; 
         nav.style.height = "200px";
         sec.style.padding = "0 0 0 0"
-        sec2.style.padding = "0 0 0 0"
+        
         sec3.style.padding = "0 0 0 0"
 
 
@@ -20,11 +19,10 @@ window.addEventListener('scroll', function(event) {
         nav.style.height = "100px";
         nav.style.transition = "all 0.5s"
         sec.style.transition = "all 0.5s"
-        sec.style.padding = "100px 0 0 0"
-        sec2.style.transition = "all 0.5s"
-        sec2.style.padding = "100px 0 0 0"
-        sec3.style.transition = "all 0.5s"
-        sec3.style.padding = "100px 0 0 0"
+        sec.style.padding = "50px 0 0 0"
+       
+         sec3.style.transition = "all 0.5s"
+         sec3.style.padding = "50px 0 0 0"
     }
 });
 
